26978e2f3d37271fe15895f4503fb6b5a47215ca
wbw520 <54616217+wbw520@users.noreply.github.com>
2023-10-08 12:50:32 +0900

    Update slots.py

--- model/contrast/slots.py
+++ model/contrast/slots.py
@@ -42,7 +42,7 @@ class ScouterAttention(nn.Module):
             q = slots
 
             dots = torch.einsum('bid,bjd->bij', q, k) * self.scale
-            dots = torch.div(dots, torch.abs(dots).sum(2).expand_as(dots.permute([2, 0, 1])).permute([1, 2, 0])) * \
+            dots = torch.div(dots, dots.sum(2).expand_as(dots.permute([2, 0, 1])).permute([1, 2, 0])) * \
                    dots.sum(2).sum(1).expand_as(dots.permute([1, 2, 0])).permute([2, 0, 1])
             attn = torch.sigmoid(dots)
 
